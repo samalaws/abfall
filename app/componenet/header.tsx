7e89d7335e3b38239a23df2d8adee7a733c75abb
Samer <samalaws@gmail.com>
2024-09-08 17:53:50 +0200

    edit css style of header componeent

--- app/componenet/header.tsx
+++ app/componenet/header.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="min-h-32 flex border-b border-gray-700  mb-5 items-center justify-between p-4">
-      <div className="flex items-center">
-        <Link href="https://www.alaws.de" passHref
+    <>
+      <header className="min-h-32 flex flex-col sm:flex-row items-center justify-between p-4">
+        <div className="flex items-center mb-4 sm:mb-0">
+          <Link
+            href="https://www.alaws.de"
+            passHref
             target="_blank"
             rel="noopener noreferrer"
             className="text-2xl font-thin hover:underline"
@@ -13,24 +16,22 @@ export default function Header() {
             <h1>
               Samer<span className="font-bold">Alaws</span>
             </h1>
-        </Link>
-      </div>
-      <div className="flex items-center">
-        <nav className="flex-grow flex justify-center space-x-8">
-          <Link href="/" passHref
-          className="text-xl font-thin hover:text-gray-400"
-          >Home
           </Link>
-          <Link href="/impressum" passHref
-            className="text-xl font-thin hover:text-gray-400"
-          >
+        </div>
+        <nav className="flex flex-col sm:flex-row justify-end items-center space-y-4 sm:space-y-0 sm:space-x-8">
+          <Link href="/" passHref className="text-xl font-thin hover:text-gray-400 transition">
+            Home
+          </Link>
+          <Link href="/impressum" passHref className="text-xl font-thin hover:text-gray-400 transition">
             Impressum
           </Link>
-        </nav>  
-        <div className="ml-4">
-          <ModeToggle />
-        </div>
-      </div>   
-    </header>
+          <div className="ml-4">
+            <ModeToggle />
+          </div>
+        </nav>
+      </header>
+      <div className="border-b border-gray-700 mt-5" />
+    </>
+    
   );
 }
